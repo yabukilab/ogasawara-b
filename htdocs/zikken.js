48c8b7f2c66300ed500117ef1fbabf8e7f17872e
ryuyakaneda <74753811+ryuyakaneda@users.noreply.github.com>
2020-12-11 13:34:27 +0900

    Add files via upload

---
+++ htdocs/zikken.js
@@ -0,0 +1,18 @@
+var widget = document.getElementById('js-filter');
+var checkboxes = widget.querySelectorAll('.filter-cond input[type="checkbox"]');
+var checkedList = [];
+var filter = function () {
+    checkedList = [];
+
+    Array.prototype.forEach.call(checkboxes, function (input) {
+        if (input.checked) {
+            checkedList.push(input.value);
+        }
+    });
+
+    widget.setAttribute('data-filter-view', checkedList.join(' '));
+};
+
+Array.prototype.forEach.call(checkboxes, function (checkbox) {
+    checkbox.addEventListener('change', filter);
+});
