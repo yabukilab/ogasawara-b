7790ab186d4b89022a30cdf9326c897d253745ec
yoshihisa-ohara <s2242026du@s.chibakoudai.jp>
2024-06-21 21:24:21 +0900

    test19

--- htdocs/hokan.php
+++ htdocs/hokan.php
@@ -1,8 +1,7 @@
 <?php
 
 # HTMLでのエスケープ処理をする関数（データベースとは無関係だが，ついでにここで定義しておく．）
-function h($var)
-{
+function h($var) {
   if (is_array($var)) {
     return array_map('h', $var);
   } else {
@@ -11,9 +10,9 @@ function h($var)
 }
 
 $dbServer = '127.0.0.1';
-$dbUser = isset($_SERVER['MYSQL_USER']) ? $_SERVER['MYSQL_USER'] : 'testuser';
+$dbUser = isset($_SERVER['MYSQL_USER'])     ? $_SERVER['MYSQL_USER']     : 'testuser';
 $dbPass = isset($_SERVER['MYSQL_PASSWORD']) ? $_SERVER['MYSQL_PASSWORD'] : 'pass';
-$dbName = isset($_SERVER['MYSQL_DB']) ? $_SERVER['MYSQL_DB'] : 'mydb';
+$dbName = isset($_SERVER['MYSQL_DB'])       ? $_SERVER['MYSQL_DB']       : 'mydb';
 
 $dsn = "mysql:host={$dbServer};dbname={$dbName};charset=utf8";
 
