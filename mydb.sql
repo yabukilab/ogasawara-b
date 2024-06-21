6bca399665b4d2ffd2c85ec016bf7087eefc0cdd
yoshihisa-ohara <s2242026du@s.chibakoudai.jp>
2024-06-21 18:18:04 +0900

    test9

--- mydb.sql
+++ mydb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- ホスト: 127.0.0.1
--- 生成日時: 2024-06-20 08:24:03
+-- 生成日時: 2024-06-21 11:17:43
 -- サーバのバージョン： 10.4.32-MariaDB
 -- PHP のバージョン: 8.2.12
 
@@ -18,8 +18,32 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- データベース: `sample`
+-- データベース: `mydb`
 --
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `mytable`
+--
+
+CREATE TABLE `mytable` (
+  `id` int(11) NOT NULL,
+  `foo` varchar(100) DEFAULT NULL,
+  `bar` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- テーブルのデータのダンプ `mytable`
+--
+
+INSERT INTO `mytable` (`id`, `foo`, `bar`) VALUES
+(2, 'い', -200),
+(3, 'う', 300),
+(5, 'い', -200),
+(6, 'う', 300),
+(8, 'い', -200),
+(9, 'う', 300);
 
 -- --------------------------------------------------------
 
@@ -53,6 +77,29 @@ INSERT INTO `posts` (`id`, `title`, `content`, `created_at`, `user_id`, `departm
 -- --------------------------------------------------------
 
 --
+-- テーブルの構造 `productinfo`
+--
+
+CREATE TABLE `productinfo` (
+  `id` int(11) NOT NULL,
+  `productname` varchar(30) NOT NULL,
+  `price` int(11) NOT NULL,
+  `stock` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- テーブルのデータのダンプ `productinfo`
+--
+
+INSERT INTO `productinfo` (`id`, `productname`, `price`, `stock`) VALUES
+(1, 'イヤホン', 1500, 100),
+(2, 'モバイルバッテリ', 3980, 10),
+(3, 'USB-TypeC接続ケーブル', 800, 50),
+(4, 'LOVOT', 500000, 2);
+
+-- --------------------------------------------------------
+
+--
 -- テーブルの構造 `sabposts`
 --
 
@@ -62,7 +109,7 @@ CREATE TABLE `sabposts` (
   `content` text NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   `user_id` int(11) DEFAULT NULL,
-  `department` varchar(255) DEFAULT NULL
+  `department` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
@@ -70,8 +117,37 @@ CREATE TABLE `sabposts` (
 --
 
 INSERT INTO `sabposts` (`id`, `title`, `content`, `created_at`, `user_id`, `department`) VALUES
-(1, 'こんにちは', 'てすと', '2024-06-14 08:56:27', 7, NULL),
-(2, 'aaa', 'sssss', '2024-06-20 05:12:51', 1, NULL);
+(1, 'こんにちは', 'てすと', '2024-06-14 08:56:27', 7, ''),
+(2, 'aaa', 'sssss', '2024-06-20 05:12:51', 1, '');
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `table1`
+--
+
+CREATE TABLE `table1` (
+  `id` int(11) NOT NULL,
+  `product` varchar(40) NOT NULL,
+  `cost` int(11) NOT NULL,
+  `stock` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- テーブルのデータのダンプ `table1`
+--
+
+INSERT INTO `table1` (`id`, `product`, `cost`, `stock`) VALUES
+(1, 'A', 1280, 1),
+(2, 'B', 2980, 0),
+(3, 'C', 198, 3),
+(4, 'D', 3980, 5),
+(5, 'E', 990, 121),
+(6, 'F', 1500, 100),
+(7, 'G', 1980, 52),
+(8, 'H', 256, 22),
+(9, 'I', 512, 27),
+(10, 'J', 3333, 4);
 
 -- --------------------------------------------------------
 
@@ -81,8 +157,8 @@ INSERT INTO `sabposts` (`id`, `title`, `content`, `created_at`, `user_id`, `depa
 
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
-  `username` varchar(50) NOT NULL,
-  `password` varchar(255) NOT NULL,
+  `username` varchar(20) NOT NULL,
+  `password` varchar(20) NOT NULL,
   `gakubu` varchar(50) NOT NULL,
   `gakka` varchar(50) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
@@ -94,17 +170,23 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `username`, `password`, `gakubu`, `gakka`, `created_at`, `department`) VALUES
-(1, 'user1', '$2y$10$favZuFHVnwvwUGFpB/5Nou1sohe.ZfHrZi4qwH9NdQ2rd0DiBDF0a', 'shakai', 'pm', '2024-06-06 05:41:55', NULL),
-(3, 'user2', '$2y$10$1/V46mFb7oqXR9kChSTleuf8gUvBYuy6wnob35/GnjT75B7gqjhve', 'shakai', 'pm', '2024-06-06 05:48:51', NULL),
+(1, 'user1', '$2y$10$favZuFHVnwvwU', 'shakai', 'pm', '2024-06-06 05:41:55', NULL),
+(3, 'user2', '$2y$10$1/V46mFb7oqXR', 'shakai', 'pm', '2024-06-06 05:48:51', NULL),
 (4, 'user3', 'pass3', 'shakai', 'pm', '2024-06-06 05:58:34', NULL),
-(5, 'user4', '$2y$10$5evIibD9LiS/Txhp4PtDRee7RLrY3Pp3iIrENFeU7cTMQMenOPQd2', 'kougaku', 'jo', '2024-06-06 06:04:12', NULL),
-(6, 'user5', '$2y$10$vGGMJcq1Lf3BRcQnoKKFJu4AT75EwHdwRsXOXqS6Qbrb6pzOV7ZRG', 'senshin', 'mirobo', '2024-06-06 06:11:23', NULL),
-(7, 'user7', '$2y$10$0fQelgSp0Bzg2Gnm1K8Nd.xo/Mnu7GKGC09Vv2uXxT1f7asrpuhr.', 'mirai', 'dejihen', '2024-06-14 08:52:36', NULL),
-(8, 'user8', '$2y$10$pDEx3Obu7dzZJsIEcbyWNuoE29mbuRZ68wLHzEofUMXoJ673.7o2q', 'shakai', 'pm', '2024-06-20 06:18:53', NULL);
+(5, 'user4', '$2y$10$5evIibD9LiS/T', 'kougaku', 'jo', '2024-06-06 06:04:12', NULL),
+(6, 'user5', '$2y$10$vGGMJcq1Lf3BR', 'senshin', 'mirobo', '2024-06-06 06:11:23', NULL),
+(7, 'user7', '$2y$10$0fQelgSp0Bzg2', 'mirai', 'dejihen', '2024-06-14 08:52:36', NULL),
+(8, 'user8', '$2y$10$pDEx3Obu7dzZJ', 'shakai', 'pm', '2024-06-20 06:18:53', NULL);
 
 --
 -- ダンプしたテーブルのインデックス
 --
+
+--
+-- テーブルのインデックス `mytable`
+--
+ALTER TABLE `mytable`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- テーブルのインデックス `posts`
@@ -113,9 +195,21 @@ ALTER TABLE `posts`
   ADD PRIMARY KEY (`id`);
 
 --
+-- テーブルのインデックス `productinfo`
+--
+ALTER TABLE `productinfo`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- テーブルのインデックス `sabposts`
 --
 ALTER TABLE `sabposts`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- テーブルのインデックス `table1`
+--
+ALTER TABLE `table1`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -130,16 +224,34 @@ ALTER TABLE `users`
 --
 
 --
+-- テーブルの AUTO_INCREMENT `mytable`
+--
+ALTER TABLE `mytable`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
 -- テーブルの AUTO_INCREMENT `posts`
 --
 ALTER TABLE `posts`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
+-- テーブルの AUTO_INCREMENT `productinfo`
+--
+ALTER TABLE `productinfo`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
 -- テーブルの AUTO_INCREMENT `sabposts`
 --
 ALTER TABLE `sabposts`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- テーブルの AUTO_INCREMENT `table1`
+--
+ALTER TABLE `table1`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- テーブルの AUTO_INCREMENT `users`
