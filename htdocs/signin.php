af8fbd049bfc3dc56de6e597939ed5ca3fc36129
yoshihisa-ohara <s2242026du@s.chibakoudai.jp>
2024-06-25 16:22:46 +0900

    test6/25 14

--- htdocs/signin.php
+++ htdocs/signin.php
@@ -2,7 +2,7 @@
 // db.php ファイルの内容
 try {
     // PDOを使用してデータベースに接続
-    $db = new PDO('mysql:host=localhost;dbname=mydb;charset=utf8', 'root', '');
+    $db = new PDO('mysql:host=localhost;dbname=mydb;charset=utf8', 'testuser', 'pass');
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     echo '接続失敗: ' . $e->getMessage();
