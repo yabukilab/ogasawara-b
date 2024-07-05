d02ac03fc0e89f8dc31bf0e4695a5481297bc227
yoshihisa-ohara <s2242026du@s.chibakoudai.jp>
2024-07-05 19:03:29 +0900

    a

--- htdocs/menu.php
+++ htdocs/menu.php
@@ -1,13 +1,8 @@
-<?php
-session_start();
-require 'db2.php';
-?>
-
 <!DOCTYPE html>
 <html>
 <head>
     <meta charset="UTF-8">
-    <title>掲示板画面</title>
+    <title>掲示板一覧</title>
     <link rel="stylesheet" href="st3.css">
 </head>
 
