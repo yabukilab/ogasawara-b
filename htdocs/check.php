2aca6a8a6b4bffc70867c2d5f5fd952952c4356f
Hideto Ogasawara <hideto.ogasawara@p.chibakoudai.jp>
2024-07-06 22:41:15 +0900

    Add files via upload

--- htdocs/check.php
+++ htdocs/check.php
@@ -7,33 +7,36 @@ if (!isset($db)) {
 
 $err_msg = ''; // エラーメッセージ変数の初期化
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $user = $_POST["username"] ?? '';
-    $pass = $_POST["password"] ?? '';
+$user = $_POST["username"];
+$pass = $_POST["password"];
 
-    if ($user && $pass) {
-        $sql = 'SELECT * FROM users WHERE username ='.'"'.$user.'"';
-        $prepare = $db->prepare($sql);
-        $prepare->execute();
-        $result = $prepare->fetch(PDO::FETCH_ASSOC);
-        if ($result) {
-            $db_pass = $result['password'];
-            if (password_verify($pass, $db_pass)) {
-                session_start();
-                $_SESSION['user_id'] = $result['id'];
-                $_SESSION['user_name'] = $result['username'];
-                $_SESSION['department_name'] = $result['department'];
-                header("Location: menu.php");
-                exit();
-            } else {
-                $err_msg = "ユーザ名またはパスワードが間違っています。";
-            }
+if ($user == '' || $pass == '') {
+    $err_msg = "ユーザ名とパスワードの両方を入力してください。";
+    header("Location: ./index.php");
+}
+
+if ($user && $pass) {
+    $sql = 'SELECT * FROM users WHERE username ='.'"'.$user.'"';
+    $prepare = $db->prepare($sql);
+    $prepare->execute();
+    $result = $prepare->fetch(PDO::FETCH_ASSOC);
+    if ($result) {
+        $db_user = $result['username'];
+        $db_pass = $result['password'];
+        if (($db_user == $user) && password_verify($pass, $db_pass)) {
+            session_start();
+            $_SESSION['user_id'] = $result['id'];
+            $_SESSION['user_name'] = $result['username'];
+            $_SESSION['department_name'] = $result['department'];
+            header("Location: ./menu.php");
+            exit();
         } else {
             $err_msg = "ユーザ名またはパスワードが間違っています。";
+            header("Location: ./index.php");
         }
     } else {
-        $err_msg = "ユーザ名とパスワードを入力してください。";
+        $err_msg = "ユーザ名が間違ってます。";
+        header("Location: ./index.php");
     }
 }
 ?>
-
