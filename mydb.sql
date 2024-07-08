3fd5daf4c3f302270d63d27a93bc786cc522aca3
kainemann <s2242075AT@s.chibakoudai.jp>
2024-07-08 13:58:50 +0900

    test7/8 8

--- mydb.sql
+++ mydb.sql
@@ -1,124 +1,106 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
+-- MariaDB dump 10.19  Distrib 10.4.32-MariaDB, for Win64 (AMD64)
 --
--- ホスト: 127.0.0.1
--- 生成日時: 2024-07-06 13:32:44
--- サーバのバージョン： 10.4.32-MariaDB
--- PHP のバージョン: 8.2.12
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
+-- Host: localhost    Database: mydb
+-- ------------------------------------------------------
+-- Server version	10.4.32-MariaDB
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- データベース: `mydb`
+-- Table structure for table `department`
 --
 
--- --------------------------------------------------------
-
---
--- テーブルの構造 `department`
---
-drop table if exists department;
-
+DROP TABLE IF EXISTS `department`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `department` (
   `ryakugo` varchar(10) DEFAULT NULL,
   `gakka` varchar(100) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- テーブルのデータのダンプ `department`
+-- Dumping data for table `department`
 --
 
-INSERT INTO `department` (`ryakugo`, `gakka`) VALUES
-('ut', '宇宙・半導体工学科'),
-('sen', '先端材料工学科'),
-('den', '電気電子工学科'),
-('jo', '情報通信システム工学科'),
-('ouyo', '応用化学科'),
-('ken', '建築学科'),
-('tosi', '都市環境工学科'),
-('deza', 'デザイン科学科'),
-('seimei', '生命科学科'),
-('tinome', '知能メディア学科'),
-('jouhou', '情報工学科'),
-('ninti', '認知情報科学科'),
-('koudo', '高度応用情報科学科'),
-('dejihen', 'デジタル変革科学科'),
-('keideza', '経営デザイン科学科'),
-('net', '情報ネットワーク学科'),
-('keijou', '経営情報科学科'),
-('pm', 'プロジェクトマネジメント学科'),
-('kinyuu', '金融・経営リスク科学科');
-
--- --------------------------------------------------------
+LOCK TABLES `department` WRITE;
+/*!40000 ALTER TABLE `department` DISABLE KEYS */;
+INSERT INTO `department` VALUES ('ut','宇宙・半導体工学科'),('sen','先端材料工学科'),('den','電気電子工学科'),('jo','情報通信システム工学科'),('ouyo','応用化学科'),('ken','建築学科'),('tosi','都市環境工学科'),('deza','デザイン科学科'),('seimei','生命科学科'),('tinome','知能メディア学科'),('jouhou','情報工学科'),('ninti','認知情報科学科'),('koudo','高度応用情報科学科'),('dejihen','デジタル変革科学科'),('keideza','経営デザイン科学科'),('net','情報ネットワーク学科'),('keijou','経営情報科学科'),('pm','プロジェクトマネジメント学科'),('kinyuu','金融・経営リスク科学科');
+/*!40000 ALTER TABLE `department` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- テーブルの構造 `posts`
+-- Table structure for table `posts`
 --
-drop table if exists posts;
 
+DROP TABLE IF EXISTS `posts`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `posts` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` int(11) NOT NULL,
   `content` text NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  `department_id` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
+  `department_id` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- テーブルの構造 `users`
+-- Dumping data for table `posts`
 --
-drop table if exists users;
 
+LOCK TABLES `posts` WRITE;
+/*!40000 ALTER TABLE `posts` DISABLE KEYS */;
+INSERT INTO `posts` VALUES (18,24,'test','2024-07-08 04:27:28','zen'),(19,24,'test','2024-07-08 04:27:37','zen'),(20,24,'自由に書き込んでください','2024-07-08 04:38:37','zen'),(21,24,'自由に書き込んでください','2024-07-08 04:38:38','zen'),(22,24,'自由に書き込んでください','2024-07-08 04:38:40','zen'),(23,24,'自由に書き込んでください','2024-07-08 04:38:40','zen'),(24,24,'自由に書き込んでください','2024-07-08 04:38:41','zen'),(25,24,'自由に書き込んでください','2024-07-08 04:38:52','zen');
+/*!40000 ALTER TABLE `posts` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `users` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(20) NOT NULL,
   `password` varchar(255) NOT NULL,
   `faculty` varchar(50) NOT NULL,
   `department` varchar(50) NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- テーブルのインデックス `posts`
---
-ALTER TABLE `posts`
-  ADD PRIMARY KEY (`id`);
-
---
--- テーブルのインデックス `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `username` (`username`);
-
---
--- ダンプしたテーブルの AUTO_INCREMENT
+-- Dumping data for table `users`
 --
 
---
--- テーブルの AUTO_INCREMENT `posts`
---
-ALTER TABLE `posts`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (22,'test1','$2y$10$saPLxmabb9fSJJg0F6AsRO1RE3oorfNvEUyOQhLSlle9c0JxIhSFC','souzou','ken','2024-07-08 03:43:03'),(23,'user1','$2y$10$dxj5bD0ks507pJey3XPxaudNcScvafp89FGYDrDe6SbLGBfm9UOEG','shakai','pm','2024-07-08 03:52:31'),(24,'teka','$2y$10$.JwQqBel7HCsMiQicpovKeBlwbX4ImCwWArahwFpU0UI5UWaxHZUq','senshin','seimei','2024-07-08 04:25:38');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
---
--- テーブルの AUTO_INCREMENT `users`
---
-ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
-COMMIT;
-
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-07-08 13:58:11
