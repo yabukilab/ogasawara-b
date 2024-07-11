141fea9ab61917a9c73f987400a91aa3e4784eee
kainemann <s2242075AT@s.chibakoudai.jp>
2024-07-11 16:11:07 +0900

    test 7/11 13

--- htdocs/index.php
+++ htdocs/index.php
@@ -11,26 +11,25 @@
     <h1>ログイン</h1>
     <form action="check.php" method="POST">
         <?php
-        if (isset($_GET['error']) && $_GET['error'] == 'blank'): ?>
-            <div style="color : red; font-weight:bold;">ユーザ名またはパスワードが間違っています</div>
-        <?php endif; ?>
+        if(isset($_GET['error']) && $_GET['error']== 'blank'):?>
+        <div style="color : red; font-weight:bold;">ユーザ名またはパスワードが間違っています</div>
+        <?php endif;?>
         <?php
-        if (isset($_GET['error']) && $_GET['error'] == 'brank'): ?>
-            <div style="color : red; font-weight:bold;">ユーザ名が間違っています</div>
-        <?php endif; ?>
+        if(isset($_GET['error']) && $_GET['error']== 'brank'):?>
+        <div style="color : red; font-weight:bold;">ユーザ名が間違っています</div>
+        <?php endif;?>
         <div class="name" style="font-size: 20px;">
             ユーザ名<input type="text" name="username" required><br>
         </div>
         <div class="password" style="font-size:20px;">
             パスワード<input type="password" name="password" required><br>
         </div>
-        <div class="loginbutton">
-            <input type="submit" name="Login" class="button" value="ログイン">
-        </div>
-    </form>
-    <div class="sinkibotan">
+        <input type="submit" name="Login" class="button" value="ログイン">
+        <div >
         <a href="signin.php">新規登録</a>
-    </div>
+        </div>        
+    </form>
+    <a href="signin.php">新規登録</a>
 </body>
 
 </html>
