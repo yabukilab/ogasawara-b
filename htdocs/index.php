916f9c4abf54c20c4d478ff49e7fd8ffe1da5034
yoshihisa-ohara <s2242026du@s.chibakoudai.jp>
2024-07-11 15:49:47 +0900

    test7/11 8

--- htdocs/index.php
+++ htdocs/index.php
@@ -11,13 +11,13 @@
     <h1>ログイン</h1>
     <form action="check.php" method="POST">
         <?php
-        if(isset($_GET['error']) && $_GET['error']== 'blank'):?>
-        <div style="color : red; font-weight:bold;">ユーザ名またはパスワードが間違っています</div>
-        <?php endif;?>
+        if (isset($_GET['error']) && $_GET['error'] == 'blank'): ?>
+            <div style="color : red; font-weight:bold;">ユーザ名またはパスワードが間違っています</div>
+        <?php endif; ?>
         <?php
-        if(isset($_GET['error']) && $_GET['error']== 'brank'):?>
-        <div style="color : red; font-weight:bold;">ユーザ名が間違っています</div>
-        <?php endif;?>
+        if (isset($_GET['error']) && $_GET['error'] == 'brank'): ?>
+            <div style="color : red; font-weight:bold;">ユーザ名が間違っています</div>
+        <?php endif; ?>
         <div class="name" style="font-size: 20px;">
             ユーザ名<input type="text" name="username" required><br>
         </div>
@@ -26,7 +26,9 @@
         </div>
         <input type="submit" name="Login" class="button" value="ログイン">
     </form>
-    <a href="signin.php">新規登録</a>
+    <div class="sinkibotan">
+        <a href="signin.php">新規登録</a>
+    </div>
 </body>
 
 </html>
