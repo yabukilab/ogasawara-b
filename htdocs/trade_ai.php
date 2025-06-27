fdb91d0cb2199c424967a1de0ec35852f6f60fbb
R2342086 <s2342086mb@s.chibakoudai.jp>
2025-06-27 17:38:08 +0900

    取引画面 DB接続設定変更

--- htdocs/trade_ai.php
+++ htdocs/trade_ai.php
@@ -6,7 +6,7 @@ ini_set('display_errors', '1');
 // DB接続設定
 $host = 'localhost';
 $db   = 'mydb';
-$user = 'testuser';
+$dbUser = isset($_SERVER['MYSQL_USER']);
 $pass = 'pass';
 $charset = 'utf8mb4';
 
