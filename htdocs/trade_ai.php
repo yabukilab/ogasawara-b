2a3e33f45f823edb6d2661b1a5c8ff92529a1a8e
R2342086 <s2342086mb@s.chibakoudai.jp>
2025-06-27 18:08:06 +0900

    接続設定更新

--- htdocs/trade_ai.php
+++ htdocs/trade_ai.php
@@ -9,7 +9,7 @@ $user   = '';
 $pass = '';
 $charset = 'utf8mb4';
 
-$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$dsn = "mysql:host={$dbServer};dbname={$dbName};charset=utf8";
 $options = [
   PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
   PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
